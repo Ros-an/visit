dea9049911f9c13d1e3c6fd92e5dc50ae51dc760
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-21 16:32:35 +0530

    added Route for PageNotFound

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import PageNotFound from "./shared/components/PageNotFound";
 
 import Users from "./users/pages/Users";
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Users />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
