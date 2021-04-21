d49c35621e79bb6a034c9e692587af5531ca198f
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-21 18:51:55 +0530

    added: route  place/new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./shared/components/PageNotFound";
+import NewPlace from "./places/pages/NewPlace";
 
 import Users from "./users/pages/Users";
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Users />} />
+      <Route path="/places/new" exact element={<NewPlace />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
