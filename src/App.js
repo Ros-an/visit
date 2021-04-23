a6da66318e43735952cad56a4436f4d8f3560659
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 16:46:15 +0530

    import: MainNavigation component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./shared/components/PageNotFound";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 import Users from "./users/pages/Users";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact element={<Users />} />
-      <Route path="/places/new" exact element={<NewPlace />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" exact element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
