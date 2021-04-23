6c72a877dc4f8f5082b5792b0634eb645b2f2f48
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 09:14:56 +0530

    changed image url

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -7,7 +7,8 @@ function Users() {
       id: 1,
       name: "roshan",
       places: 4,
-      image: "https://picsum.photos/200",
+      image:
+        "https://images.unsplash.com/photo-1619092738159-9801f5f07cbe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
     },
   ];
   return <UsersList items={USER} />;
