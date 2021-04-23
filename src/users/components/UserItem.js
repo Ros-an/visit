c8ec4422cec94460ea494a102889b583181f608f
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 09:16:08 +0530

    refactor: separated the image into avatar component

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import "./useritem.css";
 
 function UserItem(props) {
   console.log(props);
@@ -6,7 +8,7 @@ function UserItem(props) {
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.image} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
