4f7e3f26c1093232acc40fcd50292470d29e0c8a
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 08:28:10 +0530

    added: content of userItem

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function UserItem(props) {
-  return <div></div>;
+  console.log(props);
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.image} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}{" "}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default UserItem;
