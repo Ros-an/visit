4f0aae0232f5734303a16be675d22c7d8a9229c4
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 12:09:12 +0530

    refactor: passed as a child to card

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,10 +1,18 @@
 import React from "react";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+
 import "./userslist.css";
 
 function UsersList(props) {
   if (props.items.length === 0) {
-    return <div className="center">No users found.</div>;
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    );
   }
   return (
     <ul className="users-list">
