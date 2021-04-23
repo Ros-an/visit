b6cfe8573d9365f59d442b5a566d6f825192418f
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 09:15:30 +0530

    import: css file

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import UserItem from "./UserItem";
+import "./userslist.css";
 
 function UsersList(props) {
   if (props.items.length === 0) {
     return <div className="center">No users found.</div>;
   }
   return (
-    <ul className="user-list">
+    <ul className="users-list">
       {props.items.map((user) => (
         <UserItem
           key={user.id}
