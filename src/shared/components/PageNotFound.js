56c06e04e6debceb81af0e54264bbfc1e7be286c
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-23 16:44:41 +0530

    style: error message

--- src/shared/components/PageNotFound.js
+++ src/shared/components/PageNotFound.js
@@ -5,15 +5,32 @@ const customStyle = {
   display: "flex",
   justifyContent: "center",
   flexDirection: "column",
-  height: "100vh",
-  overflow: "hidden",
+  height: "60vh",
   alignItems: "center",
+  color: "white",
+};
+const errorPageStyle = {
+  maxWidth: "17rem",
   background: "white",
+  fontWeight: "500",
+  borderRadius: "0.5rem",
+  padding: "0.75rem 1rem",
+  color: "blue",
+  marginTop: "0",
+};
+
+const errorHeading = {
+  fontSize: "5rem",
+  margin: "0",
+  padding: "0",
 };
 function PageNotFound() {
   return (
     <div style={customStyle}>
-      <h1>Error 404: Page not found</h1>
+      <h1 style={errorHeading}>404</h1>
+      <h1 style={errorPageStyle}>
+        this is not the webpage you are looking for.
+      </h1>
       <Link to="/">home</Link>
     </div>
   );
