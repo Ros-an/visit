12d0791056609906fd024f557179a5a02b18e2ee
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-24 14:23:11 +0530

    passed place info to placeitem component

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,8 +1,37 @@
 import React from "react";
+import Card from "../../shared/components/UIElements/Card";
+import PlaceItem from "./PlaceItem";
+
 import "./PlaceList.css";
 
 function PlaceList(props) {
-  return <div></div>;
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>You don't have it. Maybe create one?</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          title={place.id}
+          image={place.imageUrl}
+          address={place.address}
+          description={place.description}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+        // place is created by a user and that user has an ID, thats why creatorId
+      ))}
+    </ul>
+  );
 }
 
 export default PlaceList;
