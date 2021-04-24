78a6e413460fd1996e71ed31b88a346c80775a01
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-24 14:24:20 +0530

    added route for UserPlaces component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./shared/components/PageNotFound";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 import Users from "./users/pages/Users";
 
@@ -13,7 +14,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" exact element={<Users />} />
-          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/places/new" exact element={<NewPlace />} />
+          <Route path="/:userId/places" exact element={<UserPlaces />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
